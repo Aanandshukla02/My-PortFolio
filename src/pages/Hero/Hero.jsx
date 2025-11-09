0b820ab4372d684b3bb7e846aead59f3973e66dd
Anand Shukla <aanandshukla05@gmail.com>
2025-11-09 13:16:21 +0530

    updating resume

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -162,7 +162,7 @@ const profile = {
 
                 {/* Resume Button */}
                 <a
-                  href="https://drive.google.com/file/d/11YtNSlsZ1n2SLJcxMvBMX5NprH5wf0Qp/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1dqHj0e59CKZQNcpaXIuTdIxhZNoeIa7M/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
